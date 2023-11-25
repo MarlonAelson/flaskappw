8edccfac18e24b5264660d86cad9ed2b93ee8f08
MarlonAelson <marlon.aelson.gomes@gmail.com>
2023-11-25 00:56:14 +0000

    mudanca para testar

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def index():
-    return 'Essa é minha primeira aplicação em Flask!'
+    return 'Essa é minha primeira aplicação em Flask e acabei de testar a mudança!'
 
 if __name__ == 'main':
     app.run(debug=True, host='0.0.0.0', port=1026)
